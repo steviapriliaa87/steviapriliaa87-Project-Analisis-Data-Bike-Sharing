12caeb7f95375ffae2b491dd72ea494ba102da6f
steviapriliaa87 <steviaprilia7@gmail.com>
2025-03-20 15:56:19 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -77,8 +77,15 @@ data = pd.DataFrame({"Kategori": ["Registered", "Casual"], "Jumlah": [total_regi
 
 fig = px.pie(data, names="Kategori", values="Jumlah", color="Kategori", color_discrete_map={"Registered": "darkblue", "Casual": "lightblue"}, title="Perbandingan Penyewa Registered vs Casual", hole=0.3)
 
+# Menonaktifkan label langsung, tapi tetap bisa muncul saat hover
+fig.update_traces(textinfo="none", hoverinfo="label+percent+value")
+
+st.plotly_chart(fig, use_container_width=True)
+
 #6.Penyewaan Sepeda Berdasarkan Tahun
 st.subheader("Perbandingan Tren Penyewaan Sepeda Berdasarkan Tahun")
+
+selected_compare_years = st.multiselect("Pilih Tahun untuk Dibandingkan", day_df['date'].dt.year.unique(), default=[2011, 2012])
 
 monthly_trend = day_df.groupby(["year", "month"], observed=True)["total_rentals"].sum().reset_index()
 monthly_trend["month"] = pd.Categorical(monthly_trend["month"], 
